f71573dd2962f4df6a1aa839034a225e55c6611a
Ash <contact@ash.fail>
2024-02-01 06:03:10 +0100

    Squashed 'pack/vendor/start/rainbow-delimiters.nvim/' changes from c84eeb689..ca8d5ee2b
    ca8d5ee2b Transition E2E to use the yo-dawg plugin
    6c7d91862 Move extmark assertion to testing configuration
    5941c8dbb Create test utilities
    ae0b064b0 Gracefully quit Neovim in E2E tests
    
    git-subtree-dir: pack/vendor/start/rainbow-delimiters.nvim
    git-subtree-split: ca8d5ee2b4ee1eec491040a7601d366ddc8a2e02

--- test/e2e/toggle.lua
+++ test/e2e/toggle.lua
@@ -1,99 +1,64 @@
-local say = require 'say'
-local rpcrequest = vim.fn.rpcrequest
-
-local filter = vim.fn.filter
-
-local jobopts = {
-	rpc = true,
-	width = 80,
-	height = 24,
-}
-
-local call_function = 'nvim_call_function'
-local exec_lua = 'nvim_exec_lua'
-local buf_set_lines = 'nvim_buf_set_lines'
-local buf_set_option = 'nvim_buf_set_option'
+local yd = require 'yo-dawg'
 
 describe('We can use functions to turn rainbow delimiters off and on again.', function()
 	local nvim
 
-	local function request(method, ...)
-		return rpcrequest(nvim, method, ...)
-	end
-
-	---Asserts that there are Rainbow Delimiters extmarks at the given position
-	---@param arguments integer[]  Row and column, both zero-based
-	local function has_extmarks_at(_state, arguments)
-		local row, column = arguments[1], arguments[2]
-		local nsid = request(exec_lua, 'return require("rainbow-delimiters.lib").nsids.lua', {})
-		local extmarks = request(exec_lua, 'return vim.inspect_pos(...).extmarks', {0, row, column})
-		filter(extmarks, function(_, v) return v.ns_id == nsid end)
-		return #extmarks > 0
-	end
-
-	say:set('assertion.extmarks_at.positive', 'Expected extmarks at (%s, %s)')
-	say:set('assertion.extmarks_at.negative', 'Expected no extmarks at (%s, %s)')
-	assert:register('assertion', 'extmarks_at', has_extmarks_at, 'assertion.extmarks_at.positive', 'assertion.extmarks_at.negative')
-
 	before_each(function()
-		-- Start the remote Neovim process.  The `--embed` flag lets us control
-		-- Neovim through RPC, the `--headless` flag tells it not to wait for a
-		-- UI to attach and start loading plugins and configuration immediately
-		nvim = vim.fn.jobstart({'nvim', '--embed', '--headless'}, jobopts)
-		request(exec_lua, 'the_strategy = require("rainbow-delimiters.strategy.global")', {})
-		request(exec_lua, 'TSEnsure(...)', {'lua'})
-		request(buf_set_lines, 0, 0, -1, true, {'print((((("Hello, world!")))))'})
-		request(buf_set_option, 0, 'filetype', 'lua')
+		nvim = yd.start()
+		nvim:exec_lua('the_strategy = require("rainbow-delimiters.strategy.global")', {})
+		nvim:exec_lua('TSEnsure(...)', {'lua'})
+		nvim:buf_set_lines(0, 0, -1, true, {'print((((("Hello, world!")))))'})
+		nvim:buf_set_option(0, 'filetype', 'lua')
 	end)
 
 	after_each(function()
-		vim.fn.jobstop(nvim)
+		yd.stop(nvim)
 	end)
 
 	it('Does highlighting initially', function()
-		assert.has_extmarks_at(0, 5)
+		assert.nvim(nvim).has_extmarks_at(0, 5, 'lua')
 	end)
 
 	it('Disables rainbow delimiters', function()
-		request(call_function, 'rainbow_delimiters#disable', {0})
-		assert.Not.has_extmarks_at(0, 5)
+		nvim:call_function('rainbow_delimiters#disable', {0})
+		assert.nvim(nvim).Not.has_extmarks_at(0, 5, 'lua')
 	end)
 
 	it('Remains disabled when disabling twice', function()
-		request(call_function, 'rainbow_delimiters#disable', {0})
-		request(call_function, 'rainbow_delimiters#disable', {0})
+		nvim:call_function('rainbow_delimiters#disable', {0})
+		nvim:call_function('rainbow_delimiters#disable', {0})
 
-		assert.Not.has_extmarks_at(0, 5)
+		assert.nvim(nvim).Not.has_extmarks_at(0, 5, 'lua')
 	end)
 
 	it('Turns rainbow delimiters back on', function()
-		request(call_function, 'rainbow_delimiters#disable', {0})
-		request(call_function, 'rainbow_delimiters#enable', {0})
+		nvim:call_function('rainbow_delimiters#disable', {0})
+		nvim:call_function('rainbow_delimiters#enable', {0})
 
-		assert.has_extmarks_at(0, 5)
+		assert.nvim(nvim).has_extmarks_at(0, 5, 'lua')
 	end)
 
 	it('Remains enabled when enabling twice', function()
-		request(call_function, 'rainbow_delimiters#disable', {0})
-		request(call_function, 'rainbow_delimiters#enable', {0})
-		request(call_function, 'rainbow_delimiters#enable', {0})
+		nvim:call_function('rainbow_delimiters#disable', {0})
+		nvim:call_function('rainbow_delimiters#enable', {0})
+		nvim:call_function('rainbow_delimiters#enable', {0})
 
-		assert.has_extmarks_at(0, 5)
+		assert.nvim(nvim).has_extmarks_at(0, 5, 'lua')
 	end)
 
 	it('Can be disabled after being enabled', function()
-		request(call_function, 'rainbow_delimiters#disable', {0})
-		request(call_function, 'rainbow_delimiters#enable', {0})
-		request(call_function, 'rainbow_delimiters#disable', {0})
+		nvim:call_function('rainbow_delimiters#disable', {0})
+		nvim:call_function('rainbow_delimiters#enable', {0})
+		nvim:call_function('rainbow_delimiters#disable', {0})
 
-		assert.Not.has_extmarks_at(0, 5)
+		assert.nvim(nvim).Not.has_extmarks_at(0, 5, 'lua')
 	end)
 
 	it('Can be enabled after being disabled twice', function()
-		request(call_function, 'rainbow_delimiters#disable', {0})
-		request(call_function, 'rainbow_delimiters#disable', {0})
-		request(call_function, 'rainbow_delimiters#enable', {0})
+		nvim:call_function('rainbow_delimiters#disable', {0})
+		nvim:call_function('rainbow_delimiters#disable', {0})
+		nvim:call_function('rainbow_delimiters#enable', {0})
 
-		assert.has_extmarks_at(0, 5)
+		assert.nvim(nvim).has_extmarks_at(0, 5, 'lua')
 	end)
 end)
